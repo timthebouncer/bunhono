03d5d9860c39d11ccd316eff514983cbe16aadc7
tim.wty <tim.wty@link8tech.tw>
2024-07-08 18:09:34 +0800

    vercel.json

--- backend/index.ts
+++ backend/index.ts
@@ -1,55 +1,33 @@
 import { Hono } from 'hono'
-import { handle } from "hono/vercel";
+import { cors } from 'hono/cors'
 
-export const runtime = 'edge'
+const app = new Hono()
 
-const app = new Hono().basePath("/api");
+app.use(cors())
+app.get('/', (c) => c.text('Hello Bun!'))
+    .get('/qq', (c) => {
+        const getRandomNumber = () => {
+            return Math.floor(Math.random() * 5000) + 1000
+        }
+        return c.json(
+            {
+                data: [
+                    { name: 'Jan', total: getRandomNumber() },
+                    { name: 'Feb', total: getRandomNumber() },
+                    { name: 'Mar', total: getRandomNumber() },
+                    { name: 'Apr', total: getRandomNumber() },
+                    { name: 'May', total: getRandomNumber() },
+                    { name: 'Jun', total: getRandomNumber() },
+                    { name: 'Jul', total: getRandomNumber() },
+                    { name: 'Aug', total: getRandomNumber() },
+                    { name: 'Sep', total: getRandomNumber() },
+                    { name: 'Oct', total: getRandomNumber() },
+                    { name: 'Nov', total: getRandomNumber() },
+                    { name: 'Dec', total: getRandomNumber() },
+                ],
+            },
+            200,
+        )
+    })
 
-app.get('/', (c) => {
-    return c.json(
-        {
-            totalRevenue: {
-                value: '$45,231.8',
-                sub: '+20.1% from last month',
-            },
-            subscriptions: {
-                value: '+2350',
-                sub: '+180.1% from last month',
-            },
-            sales: {
-                value: '+12,234',
-                sub: '+19% from last month',
-            },
-            activeNow: {
-                value: '+573',
-                sub: '+201 since last hour',
-            },
-        },
-        200,
-    )
-}).get('/qq', (c) => {
-    const getRandomNumber = () => {
-        return Math.floor(Math.random() * 5000) + 1000
-    }
-    return c.json(
-        {
-            data: [
-                { name: 'Jan', total: getRandomNumber() },
-                { name: 'Feb', total: getRandomNumber() },
-                { name: 'Mar', total: getRandomNumber() },
-                { name: 'Apr', total: getRandomNumber() },
-                { name: 'May', total: getRandomNumber() },
-                { name: 'Jun', total: getRandomNumber() },
-                { name: 'Jul', total: getRandomNumber() },
-                { name: 'Aug', total: getRandomNumber() },
-                { name: 'Sep', total: getRandomNumber() },
-                { name: 'Oct', total: getRandomNumber() },
-                { name: 'Nov', total: getRandomNumber() },
-                { name: 'Dec', total: getRandomNumber() },
-            ],
-        },
-        200,
-    )
-})
-
-export default handle(app);
+export default app
