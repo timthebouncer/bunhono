dc7a664b5bf5d2b14221c2fa1d266d21ddbd44ab
tim.wty <tim.wty@link8tech.tw>
2024-07-08 17:00:43 +0800

    vercel.json

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,57 @@
-//@ts-ignore
+import { Hono } from 'hono'
 
-import { Hono } from 'hono';
+const overViewApp = new Hono().get('/', (c) => {
+    return c.json(
+        {
+            totalRevenue: {
+                value: '$45,231.8',
+                sub: '+20.1% from last month',
+            },
+            subscriptions: {
+                value: '+2350',
+                sub: '+180.1% from last month',
+            },
+            sales: {
+                value: '+12,234',
+                sub: '+19% from last month',
+            },
+            activeNow: {
+                value: '+573',
+                sub: '+201 since last hour',
+            },
+        },
+        200,
+    )
+})
 
-export const config = {
-    runtime: 'edge',
-};
+const chartDataApp = new Hono().get('/', (c) => {
+    const getRandomNumber = () => {
+        return Math.floor(Math.random() * 5000) + 1000
+    }
+    return c.json(
+        {
+            data: [
+                { name: 'Jan', total: getRandomNumber() },
+                { name: 'Feb', total: getRandomNumber() },
+                { name: 'Mar', total: getRandomNumber() },
+                { name: 'Apr', total: getRandomNumber() },
+                { name: 'May', total: getRandomNumber() },
+                { name: 'Jun', total: getRandomNumber() },
+                { name: 'Jul', total: getRandomNumber() },
+                { name: 'Aug', total: getRandomNumber() },
+                { name: 'Sep', total: getRandomNumber() },
+                { name: 'Oct', total: getRandomNumber() },
+                { name: 'Nov', total: getRandomNumber() },
+                { name: 'Dec', total: getRandomNumber() },
+            ],
+        },
+        200,
+    )
+})
 
-const app = new Hono();
+const app = new Hono()
+    .route('/overview', overViewApp)
+    .route('/chartdata', chartDataApp)
 
-
-app.get('/world', async (c) => {
-    return c.json({
-        message: 'Another Route!',
-    });
-});
-
-//@ts-ignore
-// export default handle(app, '/api');
+export type AppType = typeof app
+export default app
